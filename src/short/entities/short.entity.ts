1432b2bf0c0887179eaf6dbbfc2c24a514ddbf36
harwarl <harwarl87@gmail.com>
2024-10-14 08:16:59 +0100

    chore: schema with typeorm

--- src/short/entities/short.entity.ts
+++ src/short/entities/short.entity.ts
@@ -1,1 +1,23 @@
-export class Short {}
+import { User } from 'src/user/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'url' })
+export class ShortUrl {
+  @PrimaryGeneratedColumn()
+  short_id: number;
+
+  @Column({ type: 'string' })
+  original: string;
+
+  @Column({ type: 'datetime' })
+  expires_at: Date;
+
+  @Column({ type: 'datetime' })
+  created_at: Date;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User, (user) => user.ShortUrls)
+  user: User;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,39 @@
-export class User {}
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { ShortUrl } from 'src/short/entities/short.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column({ type: 'string' })
+  username: string;
+
+  @Column({ type: 'string' })
+  email: string;
+
+  @Column({ type: 'string' })
+  passsword: string;
+
+  @Column({ type: 'timestamp' })
+  created_at: Date;
+
+  @OneToMany(() => ShortUrl, (shortUrl) => shortUrl.user_id)
+  ShortUrls: ShortUrl[];
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword(): Promise<void> {
+    if (this.passsword) {
+      this.passsword = await bcrypt.hash(this.passsword, 10);
+    }
+  }
+}
